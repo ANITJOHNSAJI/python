ea1b98d6623867e5df8deab70fb604e6d5312b30
akash2003307 <ad1821225@gmail.com>
2024-11-01 15:12:50 +0530

    first file created

---
+++ introduction/first.py
@@ -0,0 +1,20 @@
+# step-1
+# print('''hello world
+#       Bye
+#       welcome''')
+# print('helooo')
+# step-2
+# variables
+# a=10
+# print(a)
+# a=input("enter a number:")
+# print(a)
+# a=input("enter a number")
+# b=input("enter a number")
+# c=a+b
+# print(c)
+integers mathrem use cheyyan
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+c=a+b
+print(c)
