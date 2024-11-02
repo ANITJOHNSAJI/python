072ac7a1fadc888fc2650b120fd3d2c4d5355a56
akash2003307 <ad1821225@gmail.com>
2024-11-02 15:59:56 +0530

    file updated

--- introduction/first.py
+++ introduction/first.py
@@ -1,10 +1,10 @@
-# step-1
+# step-1________________________________1/11/24
 # print('''hello world
 #       Bye
 #       welcome''')
 # print('helooo')
 # step-2
-# variables
+# variables___________________________________1/11/24
 # a=10
 # print(a)
 # a=input("enter a number:")
@@ -13,8 +13,56 @@
 # b=input("enter a number")
 # c=a+b
 # print(c)
-integers mathrem use cheyyan
-a=int(input("enter a number"))
-b=int(input("enter a number"))
-c=a+b
-print(c)
+# integers mathrem use cheyyan
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# c=a+b
+# print(c)
+                                #   datatypes___________2/11/24
+# a=5
+# b=10
+# c=a+b
+# print(c)
+# a=5
+# b=10
+# c=a-b
+# print(c)
+# swapping----------------------------------------
+# a=5
+# b=10
+# a=a+b
+# b=a-b
+# a=a-b
+# print(a)
+# print(b)
+# a=10
+# a=20
+# print(a)
+# datatypes
+a=10
+print(type(a))
+a=10.2
+print(type(a))
+a=10+5j
+print(type(a))
+a='hello'
+print(type(a))
+a=['hello']
+print(type(a))
+a=('hello','hi')
+print(type(a))
+a={'hii'}
+print(type(a))
+a={'name':'anit'}
+print(type(a))
+a=True
+b=False
+print(type(a),type(b))
+
+# a=20
+# b=40
+# c=a+b
+# print(c)
+# print(A)
+# print(b)
+
