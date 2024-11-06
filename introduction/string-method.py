e1f7d99f5b34b28a67c274f87cd2a3381a27b5e1
akash2003307 <ad1821225@gmail.com>
2024-11-06 15:54:41 +0530

    file updated

--- introduction/string-method.py
+++ introduction/string-method.py
@@ -40,8 +40,11 @@
 # # Create a program that removes any leading and trailing spaces from a string provided by the user.-----------------------------
 # a="synnefo is in kochi 123"
 # c=a.split()
+# print(c)---------------------------------
+# a=input("enter a sentence")
+# b=a.split()
+# c="".join(b)
 # print(c)
-
 
 # 9.Replace a Word
 # Write a Python program that takes a string and asks the user for a word to replace, then replaces that word with a new one in the string.
@@ -58,4 +61,48 @@
 
 # 11.Join Words into a String
 # Given a list of words, write a program that joins them into a single string, with a space between each word.
+# a=input("enter a word:")
+# c=a.split()
+# d=" ".join(c)
+# print(d)
+
+
+# 12.Check if a String Contains a Substring
+# Write a Python program that asks the user for a substring and checks if it is present anywhere in the given string.
+# a=input("enter a string:")
+# b=a.split()
+# c=input("enter another string:")
+# for i in b:
+#     if i==c:
+#         print("word exist")
+#         break
+# else:
+#         print("word does not exist")
+
+
+
+# 13.Check if String is Alphanumeric
+# Write a program to check if the given string contains only alphanumeric characters (letters and numbers).
+# a="synnefoisinkochi123"
+# c=a.isalnum()
+# print(c)
+
+# 14.Check if String is Numeric
+# Ask the user for a string and check if the string contains only numeric characters (i.e., check if it’s a valid number).
+# a="123"
+# d=a.isdigit()
+# print(d)
+
+# 15.Remove All Digits from a String
+# Write a program that removes all digits from a string provided by the user.
+a=input("enter a string")
+
+ 
+ 
+
+
+
+# 16.Convert String to Title Case
+# Create a program that converts the input string to title case, where the first letter of each word is capitalized.
+
 
