27f99f5ed53d18f94f314608d44d13a1e8baa900
akash2003307 <ad1821225@gmail.com>
2024-11-14 15:48:16 +0530

    work completed

--- introduction/chr-pattern2.py
+++ introduction/chr-pattern2.py
@@ -1,14 +1,13 @@
 #1
 # a=65
 # for i in range(4):
-#     a=a-1
-#     print(end=""*a)
-#     for j in range(i+1):
-#         print(j+1,end="")
+#     for j in range(i):
+#         print(chr(a+j),end=" ")
 #     print()
+    
 # 2
 # a=65
-# for i in range(4):
+# for i in range(3):
 #     for j in range(i+1):
 #         print(chr(a),end=" ")
 #         a=a+1
@@ -24,18 +23,24 @@
 #     a=a+1
 
 #4
-# a=1
-# for i in range(3):
-#     for j in range(i+1):
-#         print(a-j,end=" ")
-#     print()
-#     a=a+1
 
 #5
+# a=65
+# for i in range(2):
+#     for j in range(3):
+#         print(chr(a+j),end=" ")
+#     print()
+#     for j in range(3):
+#         print(j+1,end=" ")
+#     print()
+
+#6
 a=65
-b=1
-for i in range(3):
+for i in range(2):
     for j in range(3):
         print(chr(a+j),end=" ")
-        print(b+j,end=" ")
     print()
+    for j in range(3):
+        print(chr(j-1),end=" ")
+    print()
+
---
+++ introduction/list.py
@@ -0,0 +1,54 @@
+# a=["apple","orange","grape",[1,2,3]]
+# for i in a:
+#     print(i)
+
+
+#empty list
+# b=[]
+# print(b)
+
+#find the length of the list
+# a=[1,2,"apple","orange"]
+# b=len(a)
+# print(b)
+
+# postionle value print cheyyan
+# a="orange"
+# print(a[1])
+
+#length kand pidikkan
+# a="orange"
+# print(len(a))
+
+# a=[1,2,"apple","orange"]
+# print(a[2])
+# b=a[2]
+# print(b)
+
+#Slicing
+
+# a=[1,2,"apple","orange"]
+# print(a[1:2])
+
+# a=[1,2,"apple","orange"]
+# print(a[1:])
+
+# a=[1,2,"apple","orange"]
+# print(a[0:])
+
+#changing
+# a=[1,2,"apple","orange"]
+# a[1]="grape"
+# print(a)
+
+# a=[1,2,"apple","orange"]
+# a[1:3]=["cherry","pineapple"]
+# print(a)
+#or
+# a=[1,2,"apple","orange"]
+# a[1:3]="cherry","pineapple"
+# print(a)
+
+# a=[1,2,"apple","orange"]
+# a[1:3]="cherry","pineapple","jackfruit"
+# print(a)
