cc1b998a495924dc8958aef1bed84130b295097a
akash2003307 <ad1821225@gmail.com>
2024-11-15 15:48:29 +0530

    list completed

--- introduction/list.py
+++ introduction/list.py
@@ -1,54 +1,88 @@
-# a=["apple","orange","grape",[1,2,3]]
-# for i in a:
-#     print(i)
+# # # # # # a=["apple","orange","grape",[1,2,3]]
+# # # # # # for i in a:
+# # # # # #     print(i)
 
 
-#empty list
-# b=[]
+# # # # # #empty list
+# # # # # # b=[]
+# # # # # # print(b)
+
+# # # # # #find the length of the list
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # b=len(a)
+# # # # # # print(b)
+
+# # # # # # postionle value print cheyyan
+# # # # # # a="orange"
+# # # # # # print(a[1])
+
+# # # # # #length kand pidikkan
+# # # # # # a="orange"
+# # # # # # print(len(a))
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # print(a[2])
+# # # # # # b=a[2]
+# # # # # # print(b)
+
+# # # # # #Slicing
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # print(a[1:2])
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # print(a[1:])
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # print(a[0:])
+
+# # # # # #changing
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # a[1]="grape"
+# # # # # # print(a)
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # a[1:3]=["cherry","pineapple"]
+# # # # # # print(a)
+# # # # # #or
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # a[1:3]="cherry","pineapple"
+# # # # # # print(a)
+
+# # # # # # a=[1,2,"apple","orange"]
+# # # # # # a[1:3]="cherry","pineapple","jackfruit"
+# # # # # # print(a)
+
+# # # # # #add list item(append)
+# # # # # a=["apple","orange","grape"]
+# # # # # a.append("melon")
+# # # # # print(a)
+
+# # # # #oro position vech add cheyunnath
+# # # # # a=["apple","orange","grape"]
+# # # # # a.insert(1,"mango")
+# # # # # print(a)
+
+# # # # a=["apple","orange","grape"]
+# # # # b=["cherry","pineapple","jackfruit"]
+# # # # # a.extend(b)
+# # # # # print(a)
+
+# # # # a=["apple","orange","grape"]
+# # # # b=["cherry","pineapple","jackfruit"]
+# # # # b.extend(a)
+# # # # print(b)
+
+# # # a=["apple","orange","grape"]
+# # # a.pop(1)
+# # # print(a)
+
+
+# # b=["cherry","pineapple","jackfruit"]
+# # b.remove("jackfruit")
+# # print(b)
+
+
+# b=["cherry","pineapple","jackfruit"]
+# del b
 # print(b)
-
-#find the length of the list
-# a=[1,2,"apple","orange"]
-# b=len(a)
-# print(b)
-
-# postionle value print cheyyan
-# a="orange"
-# print(a[1])
-
-#length kand pidikkan
-# a="orange"
-# print(len(a))
-
-# a=[1,2,"apple","orange"]
-# print(a[2])
-# b=a[2]
-# print(b)
-
-#Slicing
-
-# a=[1,2,"apple","orange"]
-# print(a[1:2])
-
-# a=[1,2,"apple","orange"]
-# print(a[1:])
-
-# a=[1,2,"apple","orange"]
-# print(a[0:])
-
-#changing
-# a=[1,2,"apple","orange"]
-# a[1]="grape"
-# print(a)
-
-# a=[1,2,"apple","orange"]
-# a[1:3]=["cherry","pineapple"]
-# print(a)
-#or
-# a=[1,2,"apple","orange"]
-# a[1:3]="cherry","pineapple"
-# print(a)
-
-# a=[1,2,"apple","orange"]
-# a[1:3]="cherry","pineapple","jackfruit"
-# print(a)
