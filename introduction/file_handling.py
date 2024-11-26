ccbe768844f2b752e221ec364ab7d8569ffedb1f
akash2003307 <ad1821225@gmail.com>
2024-11-26 15:49:49 +0530

    files added

---
+++ introduction/file_handling.py
@@ -0,0 +1,37 @@
+# f=open("example.txt",'x')
+
+# f=open("example.txt",'w')
+# f.write("hello world")
+# f.close()
+# f=open("example.txt")
+# print(f.read())
+# f.close()
+# f=open("example.txt",'a')
+# f.write("welcome to all")
+# f.close()
+# f=open("example.txt")
+# print(f.read())
+# f.close()
+
+def create():
+    f=open("example.txt",'x')
+
+def write():
+    f=open("example.txt",'w')
+    f.write("hello world")
+    f.close()
+
+def read():
+    f=open("example.txt")
+    print(f.read())
+    f.close()
+
+
+    
+
+     
+    
+    
+    
+
+
